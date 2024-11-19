27b08f8312e156d4f2d0b6aecd53de6121b4ff25
Jaganravichandran <jaganravichandran1@gmail.com>
2024-11-20 00:06:51 +0530

    Square root of number

---
+++ Basics/squareRootOfNumber.js
@@ -0,0 +1,13 @@
+// <---------x------------>
+
+// Square Root of a Number
+
+function squrtRoot(n) {
+  var sqr = parseInt(Math.sqrt(n));
+  var result = parseInt(sqr);
+  return result;
+}
+
+console.log(squrtRoot(17));
+
+// <---------x------------>
