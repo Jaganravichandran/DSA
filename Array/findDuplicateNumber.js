73a9b66d1e48dc53b8be96f8aa3684ce56066af9
Jaganravichandran <jaganravichandran1@gmail.com>
2024-12-09 23:32:49 +0530

    Find the one duplicate number in the array

---
+++ Array/findDuplicateNumber.js
@@ -0,0 +1,23 @@
+// Find the One Duplicate Number
+
+function findDuplicate(nums) {
+  let slow = nums[0];
+  let fast = nums[0];
+  do {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  } while (slow != fast);
+
+  fast = nums[0];
+  while (slow != fast) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
+  return slow;
+}
+
+const nums = [1, 2, 2, 3, 4];
+const ans = findDuplicate(nums);
+console.log(ans);
+
+// node findDuplicateNumber.js
