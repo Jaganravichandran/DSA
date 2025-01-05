a7df0ceb48bc349b3adb914545755dff6332f368
Jaganravichandran <jaganravichandran1@gmail.com>
2025-01-05 19:50:07 +0530

    Pattern Questions

--- Patterns/patterns.js
+++ Patterns/patterns.js
@@ -1,5 +1,6 @@
 // Solve any patterns
 
+// (1)
 //    * * * *
 //    * * * *
 //    * * * *
@@ -14,6 +15,7 @@ for (let i = 0; i < n; i++) {
   //   console.log(row);
 }
 
+// (2)
 //    *
 //    * *
 //    * * *
@@ -28,15 +30,13 @@ for (let i = 0; i < n; i++) {
   //   console.log(row);
 }
 
-{
-  /* (3)
-  1
-  1 2
-  1 2 3
-  1 2 3 4
-  1 2 3 4 5
-   */
-}
+// (3)
+//   1
+//   1 2
+//   1 2 3
+//   1 2 3 4
+//   1 2 3 4 5
+
 for (let i = 1; i <= n; i++) {
   let row = "";
   for (let j = 1; j <= i; j++) {
@@ -45,6 +45,7 @@ for (let i = 1; i <= n; i++) {
   //   console.log(row);
 }
 
+// (4)
 //   1
 //   2 2
 //   3 3 3
@@ -59,6 +60,7 @@ for (let i = 1; i <= n; i++) {
   //   console.log(row);
 }
 
+// (5)
 //    * * * * *
 //    * * * *
 //    * * *
@@ -73,6 +75,7 @@ for (let i = 1; i <= n; i++) {
   //   console.log(row);
 }
 
+// (6)
 //   1 2 3 4 5
 //   1 2 3 4
 //   1 2 3
@@ -87,16 +90,86 @@ for (let i = 1; i <= n; i++) {
   //   console.log(row);
 }
 
-//     *
-//    * *
-//   * * *
-//  * * * *
-// * * * * *
+// (7)
+//          *
+//        * * *
+//      * * * * *
+//    * * * * * * *
+//  * * * * * * * * *
 
-for (let i = 1; i <= n; i++) {
+for (let i = 0; i < n; i++) {
   let row = "";
-  for (let j = 1; j <= n - i + 1; j++) {
-    row += j + " ";
+  // space
+  for (let j = 0; j < n - i - 1; j++) {
+    row += "  ";
   }
-  //   console.log(row);
+  // star
+  for (let j = 0; j < 2 * i + 1; j++) {
+    row += "*" + " ";
+  }
+  // space
+  for (let j = 0; j < n - i - 1; j++) {
+    row += " ";
+  }
+  // console.log(row);
+}
+
+// (8)
+//  * * * * * * * * *
+//    * * * * * * *
+//      * * * * *
+//        * * *
+//          *
+for (let i = 0; i < n; i++) {
+  let row = "";
+  // space
+  for (let j = 0; j < i; j++) {
+    row += "  ";
+  }
+  // star (2 x n -(2 x i + 1))
+  for (let j = 0; j < 2 * n - (2 * i + 1); j++) {
+    row += "*" + " ";
+  }
+  // space
+  for (let j = 0; j < i; j++) {
+    row += " ";
+  }
+  // console.log(row);
+}
+
+// (9)
+//          *
+//        * * *
+//      * * * * *
+//    * * * * * * *
+//  * * * * * * * * *
+//  * * * * * * * * *
+//    * * * * * * *
+//      * * * * *
+//        * * *
+//          *
+
+// combine 8 and 9
+
+// (10)
+//    *
+//    * *
+//    * * *
+//    * * * *
+//    * * * * *
+//    * * * *
+//    * * *
+//    * *
+//    *
+
+for (let i = 1; i <= 2 * n - 1; i++) {
+  let row = "";
+  let stars = i;
+  if (i > n) {
+    stars = 2 * n - i;
+  }
+  for (let j = 0; j < stars; j++) {
+    row += "*";
+  }
+  // console.log(row);
 }
