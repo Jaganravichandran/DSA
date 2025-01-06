a626d5d83c1cf9b4aa6890943cbb1b0fbcd55fd7
Jaganravichandran <jaganravichandran1@gmail.com>
2025-01-06 21:30:07 +0530

    Pattern Questions

--- Patterns/patterns.js
+++ Patterns/patterns.js
@@ -6,7 +6,7 @@
 //    * * * *
 //    * * * *
 
-let n = 4;
+let n = 5;
 for (let i = 0; i < n; i++) {
   let row = ""; // initialize an empty string for each row
   for (let j = 0; j < n; j++) {
@@ -181,17 +181,17 @@ for (let i = 1; i <= 2 * n - 1; i++) {
 // 0 1 0 1
 // 1 0 1 0 1
 
-let start = 1;
+// let num = 1;
 for (let i = 0; i < n; i++) {
   let row = "";
   if (i % 2 == 0) {
-    start = 1;
+    num = 1;
   } else {
-    start = 0;
+    num = 0;
   }
   for (let j = 0; j <= i; j++) {
-    row += start + " ";
-    start = 1 - start;
+    row += num + " ";
+    num = 1 - num;
   }
   // console.log(row);
 }
@@ -204,11 +204,11 @@ for (let i = 0; i < n; i++) {
 let space = 2 * (n - 1);
 for (let i = 1; i <= n; i++) {
   let row = "";
-  let start = 1;
+  let num = 1;
   // numbers
   for (let j = 0; j < i; j++) {
-    row += start + " ";
-    start++;
+    row += num + " ";
+    num++;
   }
   // spaces
   for (let j = 1; j <= space; j++) {
@@ -216,9 +216,110 @@ for (let i = 1; i <= n; i++) {
   }
   // numbers
   for (let j = 0; j < i; j++) {
-    start--;
-    row += start + " ";
+    num--;
+    row += num + " ";
   }
   space -= 2;
-  console.log(row);
+  // console.log(row);
+}
+
+// (13)
+//  1
+//  2 3
+//  4 5 6
+//  7 8 9 10
+//  11 12 13 14 15
+
+var num = 1;
+for (let i = 1; i <= n; i++) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += num + " ";
+    num++;
+  }
+  // console.log(row);
+}
+
+// (14)
+// A
+// A B
+// A B C
+// A B C D
+// A B C D E
+
+for (let i = 1; i <= n; i++) {
+  let row = "";
+  for (let j = 0; j < i; j++) {
+    let alphabets = String.fromCharCode(65 + j);
+    row += alphabets + " ";
+  }
+  // console.log(row);
+}
+
+// (15)
+// A B C D E
+// A B C D
+// A B C
+// A B
+// A
+
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j <= n - i - 1; j++) {
+    let alphabets = String.fromCharCode(65 + j);
+    row += alphabets + " ";
+  }
+  // console.log(row);
+}
+
+// (16)
+// A
+// B B
+// C C C
+// D D D D
+// E E E E E
+
+for (let i = 0; i < n; i++) {
+  let row = "";
+  let alphabets = String.fromCharCode(65 + i);
+  for (let j = 0; j <= i; j++) {
+    row += alphabets + " ";
+  }
+  // console.log(row);
+}
+
+// (17)
+//          A
+//        A B A
+//       A B C B A
+//      A B C D C B A
+//     A B C D E D C B A
+
+for (let i = 0; i < n; i++) {
+  let row = "";
+
+  // space
+  for (let j = 0; j <= n - i - 1; j++) {
+    row += "  ";
+  }
+
+  // Characters
+  let num = 0;
+  let breakpoint = (2 * i + 1) / 2;
+  for (let j = 1; j <= 2 * i + 1; j++) {
+    let char = String.fromCharCode(65 + num);
+    if (j <= breakpoint) {
+      row += char + " ";
+      num++;
+    } else {
+      row += char + " ";
+      num--;
+    }
+  }
+
+  // space
+  for (let j = 0; j <= n - i - 1; j++) {
+    row += " ";
+  }
+  // console.log(row);
 }
