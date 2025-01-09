02eef337138390249ec808839bb17c80a148291e
Jaganravichandran <jaganravichandran1@gmail.com>
2025-01-09 21:53:04 +0530

    Armstrong Number

--- Basics/armstrongNumbers.js
+++ Basics/armstrongNumbers.js
@@ -2,23 +2,35 @@
 
 // Armstrong Numbers
 
-function armstrongNumber(n) {
-  let k = n % 100;
-  let k1 = n % 10;
-  let n1 = Math.floor(n / 100);
-  let k2 = Math.floor(k / 10);
-  let j = n1 ** 3 + k2 ** 3 + k1 ** 3;
-  if (j === n) {
-    return true;
+// function armstrongNumber(n) {
+//   let k = n % 100;
+//   let k1 = n % 10;
+//   let n1 = Math.floor(n / 100);
+//   let k2 = Math.floor(k / 10);
+//   let j = n1 ** 3 + k2 ** 3 + k1 ** 3;
+//   if (j === n) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// console.log(armstrongNumber(1634));
+
+function armstrongNumber(N) {
+  let dupNum = N;
+  let sum = 0;
+  while (N > 0) {
+    let lastDigit = N % 10;
+    N = Math.floor(N / 10);
+    sum = sum + lastDigit ** dupNum.toString().length; // (or) `${dupNum}`.length;
   }
-  return false;
+  return sum == dupNum ? true : false;
 }
 
-console.log(armstrongNumber(270));
-console.log(270 % 100);
-console.log(270 % 10);
-console.log(Math.floor(270 / 100));
-console.log(Math.floor(70 / 10));
-console.log(2 ** 3 + 7 ** 3 + 0 ** 3);
+let N = 153;
+console.log(N.toString().length);
+
+let result = armstrongNumber(N);
+console.log(result);
 
 // <---------x------------>
