327608c9b7eae6ff32930f9b27d9c06f84575839
Jaganravichandran <jaganravichandran1@gmail.com>
2025-01-18 19:00:12 +0530

    Second Largest and Smallest Element

--- Array/secondLargest.js
+++ Array/secondLargest.js
@@ -2,9 +2,46 @@
 
 // Second Largest Element in the Array
 
+// better approach
+function secondLargest(arr) {
+  let n = arr.length;
+  let largest = arr[0];
+  let secondLargest = Number.MIN_SAFE_INTEGER;
+  for (let i = 0; i < n; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    if (arr[i] > secondLargest && arr[i] != largest) {
+      secondLargest = arr[i];
+    }
+  }
+  return secondLargest;
+}
+
+function secondSmallest(arr) {
+  let n = arr.length;
+  let smallest = arr[0];
+  let secondSmallest = Number.MAX_SAFE_INTEGER;
+  for (let i = 0; i < n; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    if (arr[i] != smallest && arr[i] < secondSmallest) {
+      secondSmallest = arr[i];
+    }
+  }
+  return secondSmallest;
+}
+
+// Optimal approach
+
 function getSecondLargest(arr) {
-  var largest = -1;
-  var secondLargest = -1;
+  let largest = arr[0];
+  let secondLargest = Number.MIN_SAFE_INTEGER;
 
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > largest) {
@@ -16,5 +53,28 @@ function getSecondLargest(arr) {
   }
   return secondLargest;
 }
+
+function getSecondSmallest(arr) {
+  let n = arr.length;
+  let smallest = arr[0];
+  let secondSmallest = Number.MAX_SAFE_INTEGER;
+
+  for (let i = 0; i < n; i++) {
+    if (arr[i] < smallest) {
+      secondSmallest = smallest;
+      smallest = arr[i];
+    } else if (arr[i] != smallest && arr[i] < secondSmallest) {
+      secondSmallest = arr[i];
+    }
+  }
+  return secondSmallest;
+}
+
+let arr = [1, 2, 4, 7, 7, 5];
+// let result = secondLargest(arr);
+// let result = secondSmallest(arr);
+// let result = getSecondLargest(arr);
+let result = getSecondSmallest(arr);
+console.log(result);
 
 // <------x-----x----->
