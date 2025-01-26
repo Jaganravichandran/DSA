b2281ad19279bdaa7848478d8f474e32073314bd
Jaganravichandran <jaganravichandran1@gmail.com>
2025-01-26 16:28:00 +0530

    Sort 0's, 1's and 2's

--- Array/sort012.js
+++ Array/sort012.js
@@ -2,6 +2,32 @@
 
 // sort 0's, 1's and 2's
 
+function sortArrBetter(arr) {
+  let n = arr.length;
+  let count0 = 0;
+  let count1 = 0;
+  let count2 = 0;
+  for (let i = 0; i < n; i++) {
+    if (arr[i] == 0) {
+      count0++;
+    } else if (arr[i] == 1) {
+      count1++;
+    } else {
+      count2++;
+    }
+  }
+  for (let i = 0; i < count0; i++) {
+    arr[i] = 0;
+  }
+  for (let i = count0; i < count0 + count1; i++) {
+    arr[i] = 1;
+  }
+  for (let i = count0 + count1; i < n; i++) {
+    arr[i] = 2;
+  }
+  return arr;
+}
+
 // Dutch National Flag Algorithm
 
 function sortArr(arr) {
@@ -21,8 +47,14 @@ function sortArr(arr) {
       high--;
     }
   }
+  return arr;
 }
 
 // since it's a void we don't need to return any values
+
+let arr = [0, 1, 2, 1, 2, 0, 0, 0, 1, 2, 1];
+// let result = sortArrBetter(arr);
+let result = sortArr(arr);
+console.log(result);
 
 // <------x-----x----->
