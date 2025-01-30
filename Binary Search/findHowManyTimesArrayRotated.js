ccf5960a9dd1a225e90a9f82d00ab6ced135b5a9
Jaganravichandran <jaganravichandran1@gmail.com>
2025-01-30 23:44:00 +0530

    Find out how many times has array been rotated

--- Binary Search/findHowManyTimesArrayRotated.js
+++ Binary Search/findHowManyTimesArrayRotated.js
@@ -20,7 +20,7 @@ function findArrayRotationCount(arr) {
       break;
     }
 
-    // sorted property (1 <= 2 <= 3) => Identify which side is sorted (left/right)
+    // sorted property (1 <= 2 <= 3) => Identify which side is sorted (low/high)
     if (arr[low] <= arr[mid]) {
       if (arr[low] < ans) {
         ans = arr[low];
@@ -44,40 +44,31 @@ function findDuplicateArrayRotationCount(arr, x) {
   let n = arr.length;
   let low = 0;
   let high = n - 1;
-  let index = -1;
+
   while (low <= high) {
     let mid = Math.floor((low + high) / 2);
-    if (arr[mid] == x) {
-      index = mid;
-      return index;
-    }
-    // Edge case:
-    if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
-      low = low + 1;
-      high = high - 1;
+
+    // If mid is the smallest element
+    if (mid > 0 && arr[mid] < arr[mid - 1]) return mid;
+    if (mid < high && arr[mid] > arr[mid + 1]) return mid + 1;
+
+    // Handling duplicates
+    if (arr[low] === arr[mid] && arr[mid] === arr[high]) {
+      low++;
+      high--;
       continue;
     }
-    // identify the sorted half (left/right)
-    // sorted property (1 < 2 < 3)
+
+    // If low half is sorted, move to the high half
     if (arr[low] <= arr[mid]) {
-      if (arr[low] <= x && x <= arr[mid]) {
-        index = low;
-        high = mid - 1;
-      } else {
-        index = low;
-        low = mid + 1;
-      }
+      low = mid + 1;
     } else {
-      if (arr[mid] <= x && x <= arr[high]) {
-        index = mid;
-        low = mid + 1;
-      } else {
-        index = mid;
-        high = mid - 1;
-      }
+      // Otherwise, move to the low half
+      high = mid - 1;
     }
   }
-  return index;
+
+  return 0; // If no rotation
 }
 // let arr = [3, 4, 5, 1, 2];
 let arr = [3, 4, 5, 5, 0, 1, 2, 2, 3, 3];
