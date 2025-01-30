efd114fcced535e5387aa78344925288e498706b
Jaganravichandran <jaganravichandran1@gmail.com>
2025-01-30 14:51:25 +0530

    find first and last occurrence of x & (count)

--- Binary Search/findFirstAndLastOccurrenceOfX.js
+++ Binary Search/findFirstAndLastOccurrenceOfX.js
@@ -45,6 +45,64 @@ function findOccurrence(arr, target) {
   return [lb, upperBound(arr, target) - 1];
 }
 
-let arr = [2, 4, 6, 8, 8, 8, 11, 13];
-let result = findOccurrence(arr, 14);
+// Plain Binary Search
+
+function findOccurrenceStandard(arr, x) {
+  let first = firstOccurrence(arr, x);
+  if (first == -1) {
+    return [-1, -1];
+  }
+  let last = lastOccurrence(arr, x);
+  return [first, last];
+}
+
+function firstOccurrence(arr, x) {
+  let n = arr.length;
+  let low = 0;
+  let high = n - 1;
+  let first = -1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] == x) {
+      first = mid;
+      high = mid - 1;
+    } else if (arr[mid] < x) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return first;
+}
+
+function lastOccurrence(arr, x) {
+  let n = arr.length;
+  let low = 0;
+  let high = n - 1;
+  let last = -1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] == x) {
+      last = mid;
+      low = mid + 1;
+    } else if (arr[mid] < x) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return last;
+}
+
+// let arr = [2, 8, 8, 8, 8, 8, 11, 13];
+// let result = findOccurrence(arr, 14);
+// let result = findOccurrenceStandard(arr, 10);
+
+// follow up question => count occurrence in array (Hint => last - first + 1)
+let arr = [1, 1, 2, 2, 2, 2, 3, 3];
+let first = firstOccurrence(arr, 2);
+let last = lastOccurrence(arr, 2);
+let result = last - first + 1;
+console.log(first);
+console.log(last);
 console.log(result);
