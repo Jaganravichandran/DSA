0178181f55f19cf71751e9817a492f19ef33ee6e
Jaganravichandran <jaganravichandran1@gmail.com>
2025-02-13 22:54:51 +0530

    Power Set

--- Recursion[Patternwise]/powerSet.js
+++ Recursion[Patternwise]/powerSet.js
@@ -3,15 +3,15 @@
 // using binary digits
 function powerSet(s) {
   let n = s.length;
-  let ans = "";
-  for (let num = 0; num <= (1 << n) - 1; num++) {
-    let sub = " ";
+  let ans = [];
+  for (let num = 0; num < 1 << n; num++) {
+    let sub = "";
     for (let i = 0; i < n; i++) {
       if (num & (1 << i)) {
         sub += s[i];
       }
     }
-    ans += sub;
+    ans.push(sub);
   }
   return ans;
 }
