f90a84a5db5ad80f7420606f7d6aa622f91685b3
Jaganravichandran <jaganravichandran1@gmail.com>
2025-02-18 17:00:54 +0530

    Sudoku Solver

--- Recursion[Patternwise]/sudokuSolver.js
+++ Recursion[Patternwise]/sudokuSolver.js
@@ -1,6 +1,6 @@
 // Sudoku solver
 
-function sudokuSolver(board) {
+function sudokuSolverBrute(board) {
   let n = board.length;
   let m = board[0].length;
   for (let i = 0; i < n; i++) {
@@ -13,7 +13,7 @@ function sudokuSolver(board) {
         ) {
           if (isValid(board, i, j, c)) {
             board[i][j] = c;
-            if (sudokuSolver(board) == true) {
+            if (sudokuSolverBrute(board) == true) {
               return true;
             } else {
               board[i][j] = ".";
@@ -46,6 +46,66 @@ function isValid(board, row, col, c) {
   return true;
 }
 
+function sudokuSolverOptimal(board) {
+  let n = board.length;
+  let m = board[0].length;
+
+  // set => used to track used number
+  let rowSet = Array.from({ length: n }, () => new Set());
+  let colSet = Array.from({ length: n }, () => new Set());
+  let boxSet = Array.from({ length: n }, () => new Set());
+
+  // emptycell => used to store empty cell and fill set
+  let emptyCells = [];
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (board[i][j] == ".") {
+        emptyCells.push([i, j]);
+      } else {
+        let num = board[i][j];
+        rowSet[i].add(num);
+        colSet[j].add(num);
+        boxSet[3 * Math.floor(i / 3) + Math.floor(j / 3)].add(num);
+      }
+    }
+  }
+
+  backTrack(0, board, rowSet, colSet, boxSet, emptyCells);
+  return true;
+}
+
+function backTrack(index, board, rowSet, colSet, boxSet, emptyCells) {
+  if (index === emptyCells.length) {
+    return true;
+  }
+  let [row, col] = emptyCells[index];
+  let boxIndex = 3 * Math.floor(row / 3) + Math.floor(col / 3);
+
+  for (let num = 1; num <= 9; num++) {
+    let charNum = num.toString();
+    if (
+      !rowSet[row].has(charNum) &&
+      !colSet[col].has(charNum) &&
+      !boxSet[boxIndex].has(charNum)
+    ) {
+      board[row][col] = charNum;
+      rowSet[row].add(charNum);
+      colSet[col].add(charNum);
+      boxSet[boxIndex].add(charNum);
+
+      if (backTrack(index + 1, board, rowSet, colSet, boxSet, emptyCells))
+        return true;
+
+      board[row][col] = ".";
+      rowSet[row].delete(charNum);
+      colSet[col].delete(charNum);
+      boxSet[boxIndex].delete(charNum);
+    }
+  }
+  return false;
+}
+
 function main() {
   let board = [
     ["5", "3", ".", ".", "7", ".", ".", ".", "."],
@@ -59,10 +119,11 @@ function main() {
     [".", ".", ".", ".", "8", ".", ".", "7", "9"],
   ];
 
-  sudokuSolver(board);
-  // board.forEach((row) => console.log(row.join(" ")));
-  return board;
+  // sudokuSolverBrute(board);
+
+  sudokuSolverOptimal(board);
+  board.forEach((row) => console.log(row.join(" ")));
+  // return board;
 }
 
-let result = main();
-console.log(result);
+main();
