f26d70add85f8fbcf4432c1e536a156b21e0268f
Jaganravichandran <jaganravichandran1@gmail.com>
2025-02-21 23:43:53 +0530

    print all divisor

--- Basics/printAllDivisor.js
+++ Basics/printAllDivisor.js
@@ -1,15 +1,31 @@
 // Print all divisors
 
 function printAllDivisors(N) {
-  let row = "";
+  let row = [];
   for (let i = 1; i <= N; i++) {
     if (N % i == 0) {
-      row += i + " ";
+      row.push(i);
     }
   }
   return row;
 }
 
+function printAllDivisorsOptimal(N) {
+  let row = [];
+  for (let i = 1; i * i <= N; i++) {
+    if (N % i == 0) {
+      row.push(i);
+      if (N / i != i) {
+        row.push(N / i);
+      }
+    }
+  }
+  // sort array
+  row.sort((a, b) => a - b);
+  return row;
+}
+
 let N = 36;
-let result = printAllDivisors(N);
+// let result = printAllDivisors(N);
+let result = printAllDivisorsOptimal(N);
 console.log(result);
