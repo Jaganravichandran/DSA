1ee7037fd00a2ddaf1baa0b55fe736b938e70ac1
Jaganravichandran <jaganravichandran1@gmail.com>
2025-02-21 23:19:36 +0530

    prime number

--- Basics/primeNumber.js
+++ Basics/primeNumber.js
@@ -2,7 +2,23 @@
 
 // Prime Number
 
-function isPrime(n) {
+//Brute
+function isPrimeBrute(n) {
+  let count = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i == 0) {
+      count++;
+    }
+  }
+  if (count == 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Better
+function isPrimeBetter(n) {
   if (n === 1) {
     return 0;
   }
@@ -20,6 +36,28 @@ function isPrime(n) {
   return 1;
 }
 
-console.log(isPrime(17));
+// Optimal
+function isPrimeOptimal(n) {
+  let count = 0;
+  for (let i = 1; i * i <= n; i++) {
+    if (n % i == 0) {
+      count++;
+      if (n / i != i) {
+        count++;
+      }
+    }
+  }
+  if (count == 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let n = 10;
+let res = isPrimeBrute(n);
+// let res = isPrimeBetter(n);
+// let res = isPrimeOptimal(n);
+console.log(res);
 
 // <---------x------------>
