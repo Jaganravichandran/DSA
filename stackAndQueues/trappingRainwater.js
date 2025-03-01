9ea133855d8e9a3da98ba2ddeda470e48bbab6e6
Jaganravichandran <jaganravichandran1@gmail.com>
2025-03-01 23:21:21 +0530

    trapping rain water

--- stackAndQueues/trappingRainwater.js
+++ stackAndQueues/trappingRainwater.js
@@ -1,6 +1,6 @@
 // trapping rain water
 
-function trapBrute(arr) {
+function trapWaterBrute(arr) {
   let total = 0;
   let n = arr.length;
   for (let i = 0; i < n; i++) {
@@ -30,6 +30,72 @@ function rightMax(arr, ind) {
   return maxRight;
 }
 
+// better
+function trapWaterBetter(arr) {
+  let n = arr.length;
+  let total = 0;
+  // generating prefixMax
+  // let prefixMax = new Array(n);
+  // prefixMax[0] = arr[0];
+  // for (let i = 1; i < n; i++) {
+  //   prefixMax[i] = Math.max(prefixMax[i - 1], arr[i]);
+  // }
+
+  // generating suffixMax
+  let suffixMax = new Array(n);
+  suffixMax[n - 1] = arr[n - 1];
+  for (let i = n - 2; i >= 0; i--) {
+    suffixMax[i] = Math.max(suffixMax[i + 1], arr[i]);
+  }
+  // for (let i = 0; i < n; i++) {
+  //   let leftMax = prefixMax[i];
+  //   let rightMax = suffixMax[i];
+  //   let water = Math.min(leftMax, rightMax) - arr[i];
+  //   if (water > 0) {
+  //     total += water;
+  //   }
+  // }
+  let prefixMax = 0;
+  for (let i = 0; i < n; i++) {
+    prefixMax = Math.max(prefixMax, arr[i]);
+    let water = Math.min(prefixMax, suffixMax[i]) - arr[i];
+    if (water > 0) {
+      total += water;
+    }
+  }
+  return total;
+}
+
+// Optimal
+function trapWater(arr) {
+  let n = arr.length;
+  let lMax,
+    rMax,
+    total = 0;
+  let left = 0;
+  let right = n - 1;
+  while (left < right) {
+    if (arr[left] <= arr[right]) {
+      if (lMax > arr[left]) {
+        total += lMax - arr[left];
+      } else {
+        lMax = arr[left];
+      }
+      left++;
+    } else {
+      if (rMax > arr[right]) {
+        total += rMax - arr[right];
+      } else {
+        rMax = arr[right];
+      }
+      right--;
+    }
+  }
+  return total;
+}
+
 let arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
-let res = trapBrute(arr);
+// let res = trapWaterBrute(arr);
+// let res = trapWaterBetter(arr);
+let res = trapWater(arr);
 console.log(res);
