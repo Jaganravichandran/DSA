706f80fb3a92f1ab553715dab02133a601d01020
Jaganravichandran <jaganravichandran1@gmail.com>
2025-03-14 20:27:46 +0530

    merge overlapping intervals

--- Array/mergeIntervals.js
+++ Array/mergeIntervals.js
@@ -1,20 +1,58 @@
-// Merge Intervals
+// Merge overlapping intervals
 
-function mergeIntervals(intervals) {
-  const n = intervals.length;
+// Brute
+function mergeIntervalsBrute(arr) {
+  let n = arr.length;
+  arr.sort((a, b) => a[0] - b[0]);
+  let ans = [];
+  for (let i = 0; i < n; i++) {
+    let start = arr[i][0];
+    let end = arr[i][1];
+    if (ans.length && end <= ans[ans.length - 1][1]) {
+      continue;
+    }
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j][0] <= end) {
+        end = Math.max(end, arr[j][1]);
+      } else {
+        break;
+      }
+    }
+    ans.push([start, end]);
+  }
+  return ans;
+}
 
-  intervals.sort((a, b) => a[0] - b[0]);
+// better
+function mergeIntervalsBetter(arr) {
+  let n = arr.length;
+  arr.sort((a, b) => a[0] - b[0]);
+  let ans = [];
+  for (let i = 0; i < n; i++) {
+    if (ans.length == 0 || arr[i][0] > ans[ans.length - 1][1]) {
+      ans.push(arr[i]);
+    } else {
+      ans[ans.length - 1][1] = Math.max(ans[ans.length - 1][1], arr[i][1]);
+    }
+  }
+  return ans;
+}
 
-  const ans = [intervals[0]];
+function mergeIntervals(arr) {
+  let n = arr.length;
+
+  arr.sort((a, b) => a[0] - b[0]);
+
+  let ans = [arr[0]];
 
   for (let i = 1; i < n; i++) {
-    const last = ans[ans.length - 1];
-    const curr = intervals[i];
+    let last = ans[ans.length - 1];
+    let curr = arr[i];
 
-    if (curr[0] <= last[1]) {
-      last[1] = Math.max(last[1], curr[1]);
-    } else {
+    if (curr[0] > last[1]) {
       ans.push(curr);
+    } else {
+      last[1] = Math.max(last[1], curr[1]);
     }
   }
 
@@ -23,11 +61,17 @@ function mergeIntervals(intervals) {
 
 const arr = [
   [1, 3],
-  [8, 10],
   [2, 6],
+  [8, 9],
+  [9, 11],
+  [8, 10],
+  [2, 4],
   [15, 18],
+  [16, 17],
 ];
-const ans = mergeIntervals(arr);
+// let ans = mergeIntervalsBrute(arr);
+// let ans = mergeIntervalsBetter(arr);
+let ans = mergeIntervals(arr);
 console.log(ans);
 
 // node mergeIntervals.js
