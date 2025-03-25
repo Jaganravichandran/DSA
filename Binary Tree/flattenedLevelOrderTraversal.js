c9dad3532b3f98d24effea6d7d76ab5ce4cc72db
Jaganravichandran <jaganravichandran1@gmail.com>
2025-03-25 23:59:05 +0530

    flattened level order traversal

---
+++ Binary Tree/flattenedLevelOrderTraversal.js
@@ -0,0 +1,45 @@
+// flattened level order traversal
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function leverOrderFlatten(root) {
+  let result = [];
+  if (root == null) {
+    return result;
+  }
+  let queue = [root];
+  while (queue.length > 0) {
+    let levelSize = queue.length;
+    let currentLevel = [];
+    for (let i = 0; i < levelSize; i++) {
+      let node = queue.shift();
+
+      result.push(node.data);
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+  }
+  return result;
+}
+
+const tree = new Node(1);
+tree.left = new Node(2);
+tree.right = new Node(3);
+tree.left.left = new Node(4);
+tree.left.right = new Node(5);
+tree.right.left = new Node(6);
+tree.right.right = new Node(7);
+
+let res = leverOrderFlatten(tree);
+console.log(res);
