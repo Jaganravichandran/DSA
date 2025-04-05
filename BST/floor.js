8891b48c53f355d392f2d8a4a1e285d4dc9cd16e
Jaganravichandran <jaganravichandran1@gmail.com>
2025-04-05 22:07:53 +0530

    Floor in BST

---
+++ BST/floor.js
@@ -0,0 +1,40 @@
+// Floor in BST
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function floor(root, key) {
+  let floor = -1;
+  while (root) {
+    if (root.val == key) {
+      floor = root.val;
+      return floor;
+    }
+    if (root.val > key) {
+      root = root.left;
+    } else {
+      floor = root.val;
+      root = root.right;
+    }
+  }
+  return floor;
+}
+
+const tree = new Node(10);
+tree.left = new Node(5);
+tree.left.left = new Node(3);
+tree.left.left.left = new Node(2);
+tree.left.left.right = new Node(4);
+tree.left.right = new Node(6);
+tree.left.right.right = new Node(9);
+tree.right = new Node(13);
+tree.right.left = new Node(11);
+tree.right.right = new Node(14);
+
+let res = floor(tree, 12);
+console.log(res);
